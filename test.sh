758e102a538ebf2e5aebf10be822b54512dedbbe
cphyc <admin@cphyc.me>
2014-01-07 23:12:02 +0100

    prend comme argument type ou parse, pour éviter de se farcir tous les tests à chaque fois

--- test.sh
+++ test.sh
@@ -1,39 +1,43 @@
-#!/bin/bash
-echo "#-##############################"
-echo "--parse-only sur docs/tests/syntax/bad/*"
-for file in docs/tests/syntax/bad/*.cpp
-do
-    echo "Devrait produire une erreur :" $file
-    ./minic++ --parse-only $file
-    echo
-done
+#!/bin/sh
+if [ $1 = "parse" ]; then
+    
+    echo "#-##############################"
+    echo "--parse-only sur docs/tests/syntax/bad/*"
+    for file in docs/tests/syntax/bad/*.cpp
+    do
+	echo "Devrait produire une erreur :" $file
+	./minic++ --parse-only $file
+	echo
+    done
 
-echo
-echo "###############################"
-echo " --parse-only sur le reste"
-for file in docs/tests/syntax/good/*.cpp docs/tests/typing/*/*.cpp docs/tests/exec/*.cpp
-do
-    echo "Ne devrait pas produire d'erreur :" $file
-    ./minic++ --parse-only $file
-done
-
-echo
-echo
-echo "#-##############################"
-echo "--type-only sur docs/tests/typing/bad/*"
-for file in docs/tests/typing/bad/*.cpp
-do
-    echo "Devrait produire une erreur :" $file
-    ./minic++ --type-only $file
     echo
-done
-
-echo
-echo "###############################"
-echo " --type-only sur le reste"
-for file in docs/tests/typing/good/*.cpp docs/tests/exec/*.cpp
-do
-    echo "Ne devrait pas produire d'erreur :" $file
-    ./minic++ --type-only $file
+    echo "###############################"
+    echo " --parse-only sur le reste"
+    for file in docs/tests/syntax/good/*.cpp docs/tests/typing/*/*.cpp docs/tests/exec/*.cpp
+    do
+	echo "Ne devrait pas produire d'erreur :" $file
+	./minic++ --parse-only $file
+    done
     echo
-done
+fi
+if [ $1 == "type" ]; then 
+    echo
+    echo "#-##############################"
+    echo "--type-only sur docs/tests/typing/bad/*"
+    for file in docs/tests/typing/bad/*.cpp
+    do
+	echo "Devrait produire une erreur :" $file
+	./minic++ --type-only $file
+	echo
+    done
+    
+    echo
+    echo "###############################"
+    echo " --type-only sur le reste"
+    for file in docs/tests/typing/good/*.cpp docs/tests/exec/*.cpp
+    do
+	echo "Ne devrait pas produire d'erreur :" $file
+	./minic++ --type-only $file
+	echo
+    done
+fi
